fae81c64ee148dbfd0111782b8203a4ec1da3c0c
Grant Downs <Grant.J.Downs-1@ou.edu>
2019-04-26 17:01:41 -0500

    started method

--- leagueCodingProject/leagueCodingProject/MainWindow.xaml.cs
+++ leagueCodingProject/leagueCodingProject/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -27,13 +28,21 @@ namespace leagueCodingProject
 
         private void BtnCalculate_Click(object sender, RoutedEventArgs e)
         {
-
+            CalculateWinRate();
         }
 
         //this can probably be changed to a double but I don't want to deal with the red line right now
         public void CalculateWinRate()
         {
-            
+            string apikey = "RGAPI-a008e059-1e17-4290-ac49-758f3ccc306f";
+            using (HttpClient client = new HttpClient())
+            {
+                var response = client.GetAsync($@"https://na1.api.riotgames.com/lol/league/v4/entries/RANKED_SOLO_5x5/DIAMOND/I?api_key=RGAPI-a008e059-1e17-4290-ac49-758f3ccc306f").Result;
+                if (response.IsSuccessStatusCode)
+                {
+
+                }
+            }
         }
     }
 }
