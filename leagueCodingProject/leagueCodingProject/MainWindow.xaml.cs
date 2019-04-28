1d39c3b19a6fa7e07863f4482205f6700b68a2cc
Amy Stall <amystall@ou.edu>
2019-04-28 12:31:57 -0500

    dictionary

--- leagueCodingProject/leagueCodingProject/MainWindow.xaml.cs
+++ leagueCodingProject/leagueCodingProject/MainWindow.xaml.cs
@@ -27,11 +27,160 @@ namespace leagueCodingProject
         public MainWindow()
         {
             InitializeComponent();
+            txtResults.Visibility = Visibility.Hidden;
+            Dictionary<string, int> champions = new Dictionary<string, int>();
+            champions.Add("Aatrox", 1);
+            champions.Add("Ahri
+            champions.Add("Akali
+            champions.Add("Alistar
+            champions.Add("Amumu
+            champions.Add("Anivia
+            champions.Add("Annie
+            champions.Add("Ashe
+            champions.Add("Aurelion Sol
+            champions.Add("Azir
+            champions.Add("Bard"
+            champions.Add("Blitzcrank"
+            champions.Add("Brand"
+            champions.Add("Braum"
+            Caitlyn
+            Camille
+            Cassiopeia
+            Cho'Gath
+            Corki
+            Darius
+            Diana
+            Dr.Mundo
+            Draven
+            Ekko
+            Elise
+            Evelynn
+            Ezreal
+            Fiddlesticks
+            Fiora
+            Fizz
+            Galio
+            Gangplank
+            Garen
+            Gnar
+            Gragas
+            Graves
+            Hecarim
+            Heimerdinger
+            Illaoi
+            Irelia
+            Ivern
+            Janna
+            Jarvan IV
+            Jax
+            Jayce
+            Jhin
+            Jinx
+            Kai'Sa
+            Kalista
+            Karma
+            Karthus
+            Kassadin
+            Katarina
+            Kayle
+            Kayn
+            Kennen
+            Kha'Zix
+            Kindred
+            Kled
+            Kog'Maw
+            LeBlanc
+            Lee Sin
+            Leona
+            Lissandra
+            Lucian
+            Lulu
+            Lux
+            Malphite
+            Malzahar
+            Maokai
+            Master Yi
+            Miss Fortune
+            Mordekaiser
+            Morgana
+            Nami
+            Nasus
+            Nautilus
+            Neeko
+            Nidalee
+            Nocturne
+            Nunu & Willump
+            Olaf
+            Orianna
+            Ornn
+            Pantheon
+            Poppy
+            Pyke
+            Quinn
+            Rakan
+            Rammus
+            Rek'Sai
+            Renekton
+            Rengar
+            Riven
+            Rumble
+            Ryze
+            Sejuani
+            Shaco
+            Shen
+            Shyvana
+            Singed
+            Sion
+            Sivir
+            Skarner
+            Sona
+            Soraka
+            Swain
+            Sylas
+            Syndra
+            Tahm Kench
+            Taliyah
+            Talon
+            Taric
+            Teemo
+            Thresh
+            Tristana
+            Trundle
+            Tryndamere
+            Twisted Fate
+            Twitch
+            Udyr
+            Urgot
+            Varus
+            Vayne
+            Veigar
+            Vel'Koz
+            Vi
+            Viktor
+            Vladimir
+            Volibear
+            Warwick
+            Wukong
+            Xayah
+            Xerath
+            Xin Zhao
+            Yasuo
+            Yorick
+            Zac
+            Zed
+            Ziggs
+            Zilean
+            Zoe
+            Zyra
+
+
         }
 
         private void BtnCalculate_Click(object sender, RoutedEventArgs e)
         {
+            txtResults.Visibility = Visibility.Visible;
             CalculateWinRate();
+            
         }
 
 
---
+++ leagueCodingProject/leagueCodingProject/champtionMastery.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leagueCodingProject
+{
+    class champtionMastery
+    {
+        public Boolean chestGranted { get; set; }
+        public int championLevel { get; set; }
+        public int champtionPoints { get; set; }
+        public long championId { get; set; }
+        public long championPointsUntilNextLevel { get; set; }
+        public long lastPlayTime { get; set; }
+        public int tokensEarned { get; set; }
+        public long championPointsSinceLastLevel { get; set; }
+        public string summonerId { get; set; }
+    }
+}
